1633eb1374a76db10f8e82c99b3aefd0bbc9c635
Sameer <sameer.writes.70@gmail.com>
2022-09-04 11:21:18 +0530

    navbar

--- script.js
+++ script.js
@@ -3,6 +3,3 @@ let subMenu = document.querySelector("#subMenu");
 function toggleMenu() {
     subMenu.classList.toggle("open-menu");
 }
-
-// ----------------
-var input = document.querySelector(".input");
