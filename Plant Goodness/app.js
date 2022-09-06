34c3bbff4b68b6f33e89e979a906ddc0dbca6950
satya-subham <satyasubham85@gmail.com>
2022-09-06 16:28:31 +0530

    some add on

--- Plant Goodness/app.js
+++ Plant Goodness/app.js
@@ -104,3 +104,125 @@ preBtn.addEventListener('click', (event)=>{
 
     showPerson();
 })
+
+
+
+// ============================================= CHAT BOT =======================================
+
+const chatBotContainer = document.querySelector('.chatbot');
+const chatBotBtn = document.querySelector('#chatbot');
+
+chatBotBtn.addEventListener('click', (event)=>{
+    chatBotContainer.style.display = 'none';
+
+    const chatBotPopUpModal = document.createElement('div');
+    chatBotPopUpModal.classList.add('chatbot_popup_modal');
+    body.append(chatBotPopUpModal);
+
+
+
+    //======================== CHATBOT HEADER ====================
+
+    const chatBotHeader = document.createElement('div');
+    chatBotHeader.classList.add('chatbot_header');
+    chatBotPopUpModal.append(chatBotHeader);
+
+    const heading = document.createElement('h2')
+    heading.textContent = 'Mamaearth';
+    chatBotHeader.append(heading);
+
+    const paraImageContainer = document.createElement('div');
+    paraImageContainer.classList.add('para_image_container');
+    chatBotHeader.append(paraImageContainer);
+
+    const para = document.createElement('div');
+    para.classList.add('para');
+    para.textContent = 'Typically replies in few minutes';
+    paraImageContainer.append(para);
+    const imagesContainer = document.createElement('div');
+    imagesContainer.classList.add('images_container');
+    paraImageContainer.append(imagesContainer);
+
+    const img1 = document.createElement('img')
+    img1.src = '1.jpg';
+    imagesContainer.append(img1);
+    const img2 = document.createElement('img')
+    img2.src = '2.jpg';
+    imagesContainer.append(img2);
+    const img3 = document.createElement('img')
+    img3.src = '3.jpg';
+    imagesContainer.append(img3);
+
+
+    const downBtn = document.createElement('button');
+    downBtn.setAttribute('id', 'down_btn');
+    downBtn.innerHTML = '<i class="fa-solid fa-angle-down"></i>';
+    chatBotHeader.append(downBtn);
+
+    downBtn.addEventListener('click', (event)=>{
+        chatBotPopUpModal.remove();
+        chatBotContainer.style.display = 'block';
+    });
+
+
+
+
+    //========================= CHATBOT MAIN SECTION ==============
+
+    const chatBotMainSection = document.createElement('div');
+    chatBotMainSection.classList.add('chatbot_main_section');
+    chatBotPopUpModal.append(chatBotMainSection);
+
+    const leftCenterRight = document.createElement('div');
+    leftCenterRight.classList.add('left_center_right');
+    chatBotMainSection.append(leftCenterRight);
+
+    const left = document.createElement('div');
+    left.classList.add('chatbot_main_left');
+    leftCenterRight.append(left);
+
+    const center = document.createElement('div');
+    center.classList.add('chatbot_main_center');
+    center.textContent = 'Today';
+    leftCenterRight.append(center);
+
+    const right = document.createElement('div');
+    right.classList.add('chatbot_main_right');
+    leftCenterRight.append(right);
+
+
+    // ======================= CHATBOT FOOTER SECTION ==============
+
+    const footer = document.createElement('div');
+    footer.classList.add('chatbot_footer');
+    chatBotPopUpModal.append(footer);
+
+    
+    const footerInput = document.createElement('input');
+    footerInput.classList.add('footer_input');
+    footerInput.setAttribute('placeholder', 'Enter Your Message...')
+    footer.append(footerInput);
+
+
+    const imojiBtn = document.createElement('button');
+    imojiBtn.classList.add('imoji_btn');
+    imojiBtn.innerHTML = '<i class="fa-regular fa-face-smile"></i>';
+    footer.append(imojiBtn);
+
+    const sendBtn = document.createElement('button');
+    sendBtn.classList.add('imoji_btn');
+    sendBtn.innerHTML = '<i class="fa-regular fa-paper-plane"></i>';
+    footer.append(sendBtn);
+
+    sendBtn.addEventListener('click', (event)=>{
+
+        const pasteDiv = document.createElement('div');
+        pasteDiv.classList.add('paste_div');
+        pasteDiv.textContent = footerInput.value;
+        chatBotMainSection.append(pasteDiv);
+
+        footerInput.value = '';
+
+    })
+
+});
