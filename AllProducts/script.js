5716ddcc9838cfea6d27cc6e00c119306450bf51
karthikgeek1 <kjdnskjn>
2022-09-11 09:29:08 +0530

    login/signup

--- AllProducts/script.js
+++ AllProducts/script.js
@@ -1,28 +1,45 @@
+// Write all the selectors here
 let subMenu = document.querySelector("#subMenu");
+
+// Fetch Data from Api
+const getProductData = async (index) => {
+    const response = await axios.get(`https://mmrth-nd-api.honasa-production.net/v1/categories/${index}/products`);
+    console.log(index);
+    return response.data;
+}
+
+
+
+
 
 function toggleMenu() {
     subMenu.classList.toggle("open-menu");
 }
 
 // ----------------
+var input = document.querySelector(".input");
 const body = document.querySelector('body');
 const baby_modal = document.querySelector('.display-none');
 function babyData() {
-    
+
     baby_modal.style.display = 'flex';
     beauty.style.display = 'none';
     hair.style.display = 'none';
-    
+    // setTimeout(() => {
+    //     baby_modal.style.display = 'none';
+    // }, 10000);
 }
 
+
+
+
+
+
+
+
+
 function babyDataRemove() {
-    
     baby_modal.style.display = 'none';
-    beauty.style.display = 'none';
-    hair.style.display = 'none';
-    face.style.display = 'none';
-    bodyData.style.display = 'none';
-    
 }
 const beauty = document.querySelector('.beauty-display-none');
 function beautyData() {
@@ -30,7 +47,9 @@ function beautyData() {
     hair.style.display = 'none';
     face.style.display = 'none';
     beauty.style.display = 'flex';
-    
+    setTimeout(() => {
+        beauty.style.display = 'none';
+    }, 10000);
 }
 
 const hair = document.querySelector('.hair-display-none');
@@ -40,7 +59,9 @@ function hairData() {
     face.style.display = 'none';
     bodyData.style.display = 'none';
     hair.style.display = 'flex';
-    
+    setTimeout(() => {
+        hair.style.display = 'none';
+    }, 10000);
 }
 
 const face = document.querySelector('.face-display-none');
@@ -50,7 +71,9 @@ function faceData() {
     hair.style.display = 'none';
     bodyData.style.display = 'none';
     face.style.display = 'flex';
-
+    setTimeout(() => {
+        face.style.display = 'none';
+    }, 10000);
 }
 
 const bodyData = document.querySelector('.body-display-none');
@@ -60,47 +83,106 @@ function bodyShowData() {
     hair.style.display = 'none';
     face.style.display = 'none';
     bodyData.style.display = 'flex';
-    
+    setTimeout(() => {
+        bodyData.style.display = 'none';
+    }, 10000);
 }
 
 // main section javascript
 
 const container = document.querySelector(".container")
-window.addEventListener("DOMContentLoaded", ()=>{
+window.addEventListener("DOMContentLoaded", () => {
     fetch("https://mmrth-nd-api.honasa-production.net/v1/categories/31/products")
-    .then(res=>res.json()).then(data=>{
-        // console.log(data)
-        data.bestsellers.map((item)=>{
-            // console.log(item.images[0]);
-        productItem=`<div class="c1">
+        .then(res => res.json()).then(data => {
+            // console.log(data)
+            data.bestsellers.map((item) => {
+                // console.log(item.images[0]);
+                productItem = `<div class="c1">
             <div class="logo">Best seller</div>
             <img src=${item.images[0]} alt="mamaearth" class="img1">
             <p>${item.name}</p>
-            <p class="para"><i class="fa-solid fa-star rate"></i>${5*parseInt(item.avg_rating_percent)/100}<span> | ${item.review_count} reviews<span></p>
+            <p class="para"><i class="fa-solid fa-star rate"></i>${5 * parseInt(item.avg_rating_percent) / 100}<span> | ${item.review_count} reviews<span></p>
             <p> Rs.${item.price}</p>
             <button id=${item.id} class="butn">Add to cart</button>
         </div>`
-        container.innerHTML+=productItem
-        })
-        
-    }).catch(err=>err)
+                container.innerHTML += productItem
+            })
+
+        }).catch(err => err)
 })
 
-const allproducts = document.querySelector('.all-pro-sub')
+const productCont = document.querySelector('.all-pro-sub')
 
-window.addEventListener('DOMContentLoaded', ()=>{
+window.addEventListener('DOMContentLoaded', () => {
     fetch("https://mmrth-nd-api.honasa-production.net/v1/categories/21/products")
-    .then(res=>res.json()).then(data=> 
-        data.bestsellers.map((item)=>{
-            html=`<div class="c1">
+        .then(res => res.json()).then(data =>
+            data.bestsellers.map((item) => {
+                html = `<div class="c1">
             <div class="logo">Best seller</div>
             <img src=${item.images[0]} alt="mamaearth" class="img1">
             <p>${item.name}</p>
-            <p class="para"><i class="fa-solid fa-star rate"></i>${5*parseInt(item.avg_rating_percent)/100}<span> | ${item.review_count} reviews<span></p>
+            <p class="para"><i class="fa-solid fa-star rate"></i>${5 * parseInt(item.avg_rating_percent) / 100}<span> | ${item.review_count} reviews<span></p>
             <p> Rs.${item.price}</p>
             <button id=${item.id} class="butn">Add to cart</button>
         </div>`
-        allproducts.innerHTML+=html
-        })).catch(err=>err)
-        
+                productCont.innerHTML += html
+            })).catch(err => err)
+    // const addCartBtn = document.getElementsByClassName("butn")
+    // addCartBtn.addEventListener("click", ()=>{
+    //     alert("added to cart")
+    // })
 })
+const fileterBtn = document.querySelector(".filters");
+console.log(all);
+/* prev code
+all.addEventListener("click", ()=>{
+    fetch("https://mmrth-nd-api.honasa-production.net/v1/categories/21/products")
+    .then(res=>res.json()).then(data=>{
+        show(data.bestsellers)}).catch(err=>err)
+}) 
+*/
+
+fileterBtn.addEventListener('click', async (e) => {
+    let index = 17;
+    console.log(e.target.id);
+    switch (e.target.id) {
+        case 'all': index = 16;
+        break;
+        case 'hair': index = 27;
+        break;
+        case 'face': index = 21;
+        break;
+        case 'body': index = 29;
+        break;
+        case 'baby': index = 31;
+        break;
+        default: index = index;
+        break;
+        
+    }
+    
+    console.log(index);
+    let data = await getProductData(index);
+    console.log("data", data);
+    createProdItemCard(data.bestsellers);
+})
+
+
+function createProdItemCard(data) {
+    let prodItem;
+    productCont.innerHTML = null
+    // console.log("data", data);
+    data.map((e) => {
+        console.log("each", e.images[0]);
+        prodItem = `<div class="c1">
+            <div class="logo">Best seller</div>
+            <img src=${e.images[0]} alt="mamaearth" class="img1">
+            <p>${e.name}</p>
+            <p class="para"><i class="fa-solid fa-star rate"></i>${5 * parseInt(e.avg_rating_percent) / 100}<span> | ${e.review_count} reviews</span></p>
+            <p> Rs.${e.price}</p>
+            <button id=${e.id} class="butn">Add to cart</button>
+        </div>`
+        productCont.innerHTML += prodItem
+
+    })
+}
--- Nav/app.js
+++ Nav/app.js
@@ -1,60 +1,51 @@
-let subMenu = document.querySelector("#subMenu");
-
-function toggleMenu() {
-  subMenu.classList.toggle("open-menu");
-}
-
 const body = document.querySelector("body");
-const baby_modal = document.querySelector(".display-none");
+const babyModal = document.querySelector(".display-none");
+const beauty = document.querySelector(".beauty-display-none");
+const hair = document.querySelector(".hair-display-none");
+const face = document.querySelector(".face-display-none");
+const bodyData = document.querySelector(".body-display-none");
+
 function babyData() {
-  baby_modal.style.display = "flex";
+  babyModal.style.display = "flex";
   beauty.style.display = "none";
   hair.style.display = "none";
 }
-
 function babyDataRemove() {
-  baby_modal.style.display = "none";
+  babyModal.style.display = "none";
   beauty.style.display = "none";
   hair.style.display = "none";
   face.style.display = "none";
   bodyData.style.display = "none";
 }
-const beauty = document.querySelector(".beauty-display-none");
 function beautyData() {
-  baby_modal.style.display = "none";
+  babyModal.style.display = "none";
   hair.style.display = "none";
   face.style.display = "none";
   beauty.style.display = "flex";
 }
-
-const hair = document.querySelector(".hair-display-none");
 function hairData() {
-  baby_modal.style.display = "none";
+  babyModal.style.display = "none";
   beauty.style.display = "none";
   face.style.display = "none";
   bodyData.style.display = "none";
   hair.style.display = "flex";
 }
-
-const face = document.querySelector(".face-display-none");
 function faceData() {
-  baby_modal.style.display = "none";
+  babyModal.style.display = "none";
   beauty.style.display = "none";
   hair.style.display = "none";
   bodyData.style.display = "none";
   face.style.display = "flex";
 }
-
-const bodyData = document.querySelector(".body-display-none");
 function bodyShowData() {
-  baby_modal.style.display = "none";
+  babyModal.style.display = "none";
   beauty.style.display = "none";
   hair.style.display = "none";
   face.style.display = "none";
   bodyData.style.display = "flex";
 }
 
-// main section javascript
+//===================== main section javascript =================
 
 const container = document.querySelector(".container");
 window.addEventListener("DOMContentLoaded", () => {
@@ -101,32 +92,132 @@ window.addEventListener("DOMContentLoaded", () => {
       })
     )
     .catch((err) => err);
-
+  const butn = document.getElementsByClassName("butn");
+  const sample = Array.from(butn);
+  // console.log(butn);
 });
-
-
-
-
-
-
-
-
-
 
 //================================================= LOG IN ========================================
 
+const subMenuWrap = document.querySelector(".sub-menu-wrap");
+const loginBtn = document.querySelector(".logbut");
+const crossBtn = document.querySelector("#cross");
+const header = document.querySelector("header");
+const main = document.querySelector("main");
+const loginWithOtpBtn = document.querySelector("#login_with_otp");
+const loginPhoneMunberInput = document.querySelector("#login_input");
+const signUpModal = document.querySelector(".signup_popup");
+const loginModal = document.querySelector(".login_popup");
+const signUpNumber = document.querySelector("#signup_phone_number");
+const loginNumber = document.querySelector("#login_input");
+const signUpBtn = document.querySelector("#signup_btn");
+const signInModal = document.querySelector('.signin_popup');
 
 
-const subMenuWrap = document.querySelector('.sub-menu-wrap');
+function toggleMenu() {
+  subMenuWrap.style.display = "block";
+}
 
-const loginPopUp = document.querySelector('.login_popup');
+function toggleMenuRemove() {
+  subMenuWrap.style.display = "none";
+}
 
-const loginBtn = document.querySelector('.logbut');
-
-const crossBtn = document.querySelector('#cross');
+const loginPopUpMainContainer = document.querySelector(
+  ".login_popup_main_container"
+);
 
 function login() {
-    loginPopUp.remove();
+  subMenuWrap.style.display = "none";
+  main.classList.toggle("active");
+  header.classList.toggle("active");
+  loginPopUpMainContainer.style.display = "block";
+}
+
+function loginPopupRemove() {
+  loginPopUpMainContainer.style.display = "none";
+  main.classList.toggle("active");
+  header.classList.toggle("active");
+}
+
+function loginWithOtp() {
+  if (loginPhoneMunberInput.value.length > 1) {
+    loginModal.remove();
+    signUpModal.style.display = "block";
+    signUpNumber.value = loginNumber.value;
+    loginPopUpMainContainer.append(signUpModal);
+  }
+}
+
+function backToLogin() {
+  signUpModal.remove();
+  loginPopUpMainContainer.append(loginModal);
 }
 
 
+const userData = [];
+signUpBtn.addEventListener("click", (event) => {
+  const fName = document.querySelector("#signup_first_name").value;
+  const lName = document.querySelector("#signup_last_name").value;
+  const eMail = document.querySelector("#signup_email_id").value;
+
+  if (!eMail.endsWith("@gmail.com")) {
+    alert("Invalid Email");
+  }
+else{
+
+  const search = userData.find((x) => {
+    return x.userEmail == eMail;
+  });
+  
+  if (search == undefined) {
+    let userDetails = {
+      userFirstName: fName,
+      userLastName: lName,
+      userEmail: eMail,
+    };
+    userData.push(userDetails);
+    localStorage.setItem('userData',JSON.stringify(userData))
+    
+  } else {
+    alert("user already exist");
+  }
+  console.log(userData);
+  signUpModal.remove();
+  loginPopUpMainContainer.append(signInModal)
+}
+
+});
+
+const signInCrossBtn = document.querySelector('#signin_cross');
+signInCrossBtn.addEventListener('click', (event)=>{
+  signInModal.remove();
+  loginPopUpMainContainer.append(loginModal)
+  loginPopUpMainContainer.style.display = "none";
+  main.classList.toggle("active");
+  header.classList.toggle("active");
+})
+
+
+const signInBtn = document.querySelector('#signin_btn');
+const logedname = document.querySelector(".logedname")
+
+signInBtn.addEventListener('click', ()=>{
+  const signInEmail = document.querySelector('#signin_email').value;
+
+  for(let i=0; i<userData.length; i++){
+    if (userData[i].userEmail == signInEmail) {
+      // console.log("userEmail", userData[i].userEmail);
+      // console.log("singInemail", signInEmail);
+      loginPopUpMainContainer.style.display = "none";
+    main.classList.toggle("active");
+    header.classList.toggle("active");
+    // signUpBtn.innerHTML="LogOut"
+    loginBtn.innerHTML="log out"
+    logedname.innerHTML=`welcome ${userData[i].userFirstName}`
+    // console.log();
+  }
+  // else if(userData[i].userEmail!=signInEmail){
+  //   alert('Please Sign Up')
+  // }
+}
+})
